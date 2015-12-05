1c18176d91c18a359afc8f5db4c37499689126c1
karstendick <joshdick@gmail.com>
2015-12-05 10:59:34 -0500

    added logic to count neighbors

--- src/life/core.clj
+++ src/life/core.clj
@@ -20,3 +20,34 @@
         
         :else
         dead))
+
+(defn cell-at
+  "Takes a vector of vectors and returns the value at
+  the given row and column"
+  [vv r c]
+  (get-in vv [r c]))
+
+(defn in-bounds?
+  [r c num-rows num-cols]
+  (and (<= 0 r (dec num-rows))
+       (<= 0 c (dec num-cols))))
+
+(defn cell-at-live?
+  [board r c num-rows num-cols]
+  (and (in-bounds? r c num-rows num-cols)
+       (live? (cell-at board r c))))
+
+(defn count-neighbors
+  ([board r c num-rows num-cols]
+   (count-neighbors board r c num-rows num-cols cell-at-live?))
+  ([board r c num-rows num-cols pred?]
+   (count
+    (filter true?
+            [(pred? board r (dec c) num-rows num-cols)
+             (pred? board r (inc c) num-rows num-cols)
+             (pred? board (dec r) c num-rows num-cols)
+             (pred? board (inc r) c num-rows num-cols)
+             (pred? board (inc r) (inc c) num-rows num-cols)
+             (pred? board (dec r) (inc c) num-rows num-cols)
+             (pred? board (inc r) (dec c) num-rows num-cols)
+             (pred? board (dec r) (dec c) num-rows num-cols)]))))
